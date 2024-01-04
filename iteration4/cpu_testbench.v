fb48b6310ee79ecc453421ee4252bfdc02c61154
Arda Öztürk <143508784+ArdaOzturk192@users.noreply.github.com>
2024-01-04 14:28:07 +0300

    pc fixed

--- iteration4/cpu_testbench.v
+++ iteration4/cpu_testbench.v
@@ -30,8 +30,7 @@ module cpu_testbench;
         reset = 1;
 
         // Apply a reset pulse
-        #20 reset = 0;
-        #20 reset = 1;
+        #50 reset = 0;
 
         // Initialize registers
 	myCpu.my_reg_file.registers[1] = 0;
@@ -40,8 +39,8 @@ module cpu_testbench;
 
 
         // Load a test program into instruction memory
-	myCpu.my_instr_memory.instr_mem[0] = 18'h1123; // ADD R1, R2, R3
- 	myCpu.my_instr_memory.instr_mem[1] = 18'h2120A; // ADDI R1, R2, #10        
+	myCpu.my_instr_memory.instr_mem[0] = 18'h08004; // ADD R1, R2, R3
+ 	myCpu.my_instr_memory.instr_mem[1] = 18'h24000; // ADDI R1, R2, #10        
 
 
 	// Wait for a few clock cycles after reset
@@ -51,18 +50,18 @@ module cpu_testbench;
     	$display("Initial state: PC = %d, R1 = %d, R2 = %d, R3 = %d", myCpu.program_counter, myCpu.my_reg_file.registers[1], myCpu.my_reg_file.registers[2], myCpu.my_reg_file.registers[3]);
 	
 	// Run the simulation for a certain period
-        #10;
+        #20;
 	 // Manually control the program counter
 
 
 
   	        // Check outputs and internal states
            	// we can adjust the timing (#50, #100, etc.) according to WHATEVER clock period and or instruction execution time. cheers.
-   	#100 $display("After first instruction: PC = %d, R1 = %d, R2 = %d, R3 = %d", myCpu.program_counter, myCpu.my_reg_file.registers[1], myCpu.my_reg_file.registers[2], myCpu.my_reg_file.registers[3]);
-    	#100 $display("After second instruction: PC = %d, R1 = %d, R2 = %d, R3 = %d", myCpu.program_counter, myCpu.my_reg_file.registers[1], myCpu.my_reg_file.registers[2], myCpu.my_reg_file.registers[3]);
+   	#10 $display("After first instruction: PC = %d, R1 = %d, R2 = %d, R3 = %d", myCpu.program_counter, myCpu.my_reg_file.registers[1], myCpu.my_reg_file.registers[2], myCpu.my_reg_file.registers[3]);
+    	#10 $display("After second instruction: PC = %d, R1 = %d, R2 = %d, R3 = %d", myCpu.program_counter, myCpu.my_reg_file.registers[1], myCpu.my_reg_file.registers[2], myCpu.my_reg_file.registers[3]);
 	
 	// Wait for a few clock cycles after loading instructions
-    	#100;
+    	#10;
     	$display("After instruction fetch: PC = %d, current_instr = %h", myCpu.program_counter, myCpu.current_instr);
     	$display("Control signals: pc_write = %b, branch = %b", myCpu.my_cu.pc_write, myCpu.my_cu.branch);
 	$display("After first instruction: PC = %d, Opcode = %b, PC Write = %b, Branch = %b", myCpu.program_counter, debug_opcode, debug_pc_write, debug_branch);
@@ -74,3 +73,4 @@ module cpu_testbench;
         $finish;
     end
 endmodule
+
